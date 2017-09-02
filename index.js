c8eef59bcd094dcde318cc2ab95f59f5ecbc8c59
Brandon Chan <iambrandonchan@gmail.com>
2017-09-02 16:12:59 -0500

    implemented a route, a brief skeleton of the prompts, and a button

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-var blogApp = angular.module("blogApp", [])
+var blogApp = angular.module("blogApp", ['ui.router'])
 
 .controller("blogController", function($scope) {
     $scope.name = "Brandon";
+    $scope.week1 = false;
+    $scope.week1Content = [{
+        prompt: 'What are my expectations of the class?',
+        response: ''
+    }, {
+        prompt: 'What did you do this past week?',
+        response: ''
+    }, {
+        prompt: "What's in my way?",
+        response: ''
+    }, {
+        prompt: 'What will I do next week?',
+        response: ''
+    }, {
+        prompt: "What's my experience of the class?",
+        response: ''
+    }, {
+        prompt: "What's my pick-of-the-week or tip-of-the-week?",
+        response: ''
+    }]
 
+})
 
+.config(function($stateProvider) {
+    var week1State = {
+        name: 'week1Blog',
+        url: '/week1',
+        template: '<h3>hello everyone! {{week1Content[0]}}</h3>'
+    }
+
+    // var week2State = {
+    //     name: 'about',
+    //     url: '/about',
+    //     template: '<h3>Its the UI-Router hello world app!</h3>'
+    // }
+
+    $stateProvider.state(week1State);
+    // $stateProvider.state(week2State);
 })
